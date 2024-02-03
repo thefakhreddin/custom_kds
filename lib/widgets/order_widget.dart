3af03c4ec583dda6fa25ce5bc631bf346e69196a
thefakhreddin <fakhredin0098@gmail.com>
2024-02-02 18:34:48 -0800

    the info on order is consice now. only the order name and number

--- lib/widgets/order_widget.dart
+++ lib/widgets/order_widget.dart
@@ -8,6 +8,11 @@ class OrderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Create a string that concatenates all item names and quantities
+    final itemsString = order.items
+        .map((item) => '${item.quantity}x ${item.name}')
+        .join('\n'); // Each item on a new line
+
     return Container(
       margin: EdgeInsets.all(8),
       padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
@@ -23,29 +28,13 @@ class OrderWidget extends StatelessWidget {
           ),
         ],
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Order ID: ${order.id}',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 5),
-          Text(
-            'Ordered At: ${order.createdAt}',
-            style: TextStyle(fontSize: 14, color: Colors.grey[700]),
-          ),
-          SizedBox(height: 5),
-          Text(
-            'Status: ${order.state}',
-            style: TextStyle(fontSize: 14, color: Colors.blueAccent),
-          ),
-          SizedBox(height: 10),
-          ...order.items.map((item) => Text(
-                '${item.quantity}x ${item.name}',
-                style: TextStyle(fontSize: 14),
-              )),
-        ],
+      child: Text(
+        itemsString,
+        style: TextStyle(
+          fontSize: 18, // Use a larger font size for better readability
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
