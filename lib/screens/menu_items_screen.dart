aaa2adf63f15e2cfa1a1bd3f216cfa3cbaa67898
thefakhreddin <fakhredin0098@gmail.com>
2024-04-18 22:48:54 -0700

    menu items have check boxes now

--- lib/screens/menu_items_screen.dart
+++ lib/screens/menu_items_screen.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
 
-class MenuItemsScreen extends StatelessWidget {
+class MenuItemsScreen extends StatefulWidget {
   final List<String> menuItems;
 
   MenuItemsScreen({Key? key, required this.menuItems}) : super(key: key);
 
   @override
+  _MenuItemsScreenState createState() => _MenuItemsScreenState();
+}
+
+class _MenuItemsScreenState extends State<MenuItemsScreen> {
+  late List<bool> _selected; // Declare as late
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize _selected based on the length of menuItems
+    _selected = List<bool>.filled(widget.menuItems.length, false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: menuItems.length,
+      itemCount: widget.menuItems.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(menuItems[index]),
+        return CheckboxListTile(
+          title: Text(widget.menuItems[index]),
           tileColor: Colors.white,
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          value: _selected[index],
+          onChanged: (bool? value) {
+            setState(() {
+              _selected[index] = value!;
+            });
+          },
+          controlAffinity: ListTileControlAffinity.leading,
         );
       },
     );
